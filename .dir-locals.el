db11806cdff0a29eb0ed4c05d3650c603d380cb1
David Ben-Yaakov <david@tangogolfdigital.com>
2021-10-31 15:39:36 -0500

    Updated to Curses 1.0.5

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((swift-mode
-  (flycheck-swift-include-search-paths  "/usr/local/lib/merlin/CNCURSES-1.0.2/CNCURSES" "/usr/local/lib/merlin/Curses-1.0.3/Curses/.build/debug")))
